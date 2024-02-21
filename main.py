679c026bb51241e69cd2d0413379f9ec7d6e1349
Anish <anishgharti10@gmail.com>
2024-02-21 09:25:19 +0545

    websocket implemented

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from db.database import engine
 from db import models
 from templates import templates
 from exceptions import StoryException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
-
+import time
+from client import html
+from fastapi.websockets import WebSocket
 app = FastAPI()
 app.include_router(templates.router)
 app.include_router(authentication.router)
@@ -22,6 +23,15 @@ app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
 
+
+# @app.middleware('http')
+# async def  add_middlewar(request: Request, call_next):
+#     start_time = time.time()
+#     response =  await call_next(request)
+#     duration = time.time() - start_time
+#     response.headers['duaration'] = str(duration)
+#     return response
+
 @app.get("/hello")
 def index():
     return {"message": "Hello world!"}
@@ -30,6 +40,29 @@ def index():
 @app.exception_handler(StoryException)
 def story_exception_handler(request: Request, exc: StoryException):
     return JSONResponse(status_code=418, content={"detail": exc.name})
+
+
+
+
+@app.get('/')
+async def get():
+    return HTMLResponse(html)
+
+
+client = []
+
+
+@app.websocket('/chat')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    client.append(websocket)
+    while True:
+        data = await  websocket.receive_text()
+        for clien in client:
+            await clien.send_text(data)
+
+
+
 
 
 # @app.exception_handler(HTTPException)
--- templates/templates.py
+++ templates/templates.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
-
+from custom_log import log
 from schemas import ProductBase
 router = APIRouter(prefix='/templates', tags=['Templates'])
 
@@ -10,7 +10,8 @@ router = APIRouter(prefix='/templates', tags=['Templates'])
 templates = Jinja2Templates(directory='templates') 
 
 @router.post('/products/{id}', response_class=HTMLResponse)
-def get_product(id: str, product: ProductBase, request: Request):
+def get_product(id: str, product: ProductBase, request: Request, bt: BackgroundTasks):
+    bt.add_task(log_template_call, f'product with id {id} is run on back')
     return templates.TemplateResponse(
         "product.html", {
             'request': request,
@@ -20,3 +21,7 @@ def get_product(id: str, product: ProductBase, request: Request):
             'price': product.price,
         }
     )
+    
+
+def log_template_call(message: str):
+    log("My api", message)   
